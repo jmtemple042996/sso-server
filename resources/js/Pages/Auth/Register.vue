44c8723fa166937b7dd06c46d8666bd3ad12e2df
James Matthew Temple <jmtemple0429@gmail.com>
2025-10-02 09:42:03 -0400

    add padding to login page

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -47,7 +47,7 @@ const submit = () => {
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
-                        <div>
+                        <div class="mt-4">
                 <InputLabel for="name" value="Last Name" />
                 <TextInput
                     id="name"
