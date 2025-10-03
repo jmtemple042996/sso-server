73298a0e4832e792c2addf277afe92130c38a832
James Matthew Temple <jmtemple0429@gmail.com>
2025-10-02 21:37:06 -0400

    Add combined name

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -13,6 +13,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
 use Laravel\Fortify\Fortify;
+use App\Models\User;
 
 class FortifyServiceProvider extends ServiceProvider
 {
