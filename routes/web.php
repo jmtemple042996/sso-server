729cb7492c9f018f87634eed2d84382b6122fcb9
James Matthew Temple <jmtemple0429@gmail.com>
2025-10-04 15:50:20 -0400

    add services route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
+    Route::resources('services',ServiceController::class);
+
 });
